21eefa69cf6219c0c0526d25b4ef9ac46dd19f1b
Enrique Sotelo Ponce <sotelo.enrique@gmail.com>
2022-07-07 19:24:03 -0500

    Variables - ejemplo de uso

---
+++ variables/creating_variables.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+clear                                       #Limpiar pantalla
+echo "SCRIPT BEGINS"                        #
+echo "Hello $LOGNAME!"                      #Imprimir variable de entorno
+echo
+
+echo "Today's date and time"
+date                                        #Imprimir fecha y hora
+echo
+
+my_num=50                                   #Declarar variables de usuario
+my_day="Sunday"
+
+echo "The value of my_num is $my_num"       #Imprimir variables de usuario
+echo "The value of my_day is $my_day"
+echo
+
+echo "SCRIPT FINISHED!!"
+echo
