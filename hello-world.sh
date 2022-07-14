7a1d5a0977166ee24dcfb40039b75c423888acaa
Enrique Sotelo Ponce <sotelo.enrique@gmail.com>
2022-07-14 11:06:43 -0500

    Ejemplos - Hola mundo

---
+++ hello-world.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Este es un comentario
+echo "Hello $LOGNAME, have a nice day!!"
+echo "You are working in directory `pwd`."
+echo "You are working on a machine called `uname -o`."
+echo "List of files in your directory is:" 
+ls
+echo "Bye for now $LOGNAME. The time is `date +%T`!"
