c7b8ac7e17c3f984af6b4e1a9c3222eefd2d3f18
Enrique Sotelo Ponce <sotelo.enrique@gmail.com>
2022-07-26 13:54:09 -0500

    Ejemplos - Mostrar la branch de git en la terminal

---
+++ show-git-branch.sh
@@ -0,0 +1,8 @@
+# Git - Mostrar branch actual
+# Esta código se pega en el archivo ~/.bashrc para mostrar la branch activa del repositorio
+#
+parse_git_branch() {
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+
+export PS1="\u@\h \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
